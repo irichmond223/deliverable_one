c222f9c2dfdaa1407b08cfd7c7407f61af863f83
ilona <ilonabarashkina@gmail.com>
2019-09-15 13:57:17 -0400

    First Commit

---
+++ deliverable1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace deliverable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Totalnumber1,Totalnumber2,Totalnumber3;
+            double total, average;
+
+
+             Console.WriteLine("Enter a number 1: ");
+            Totalnumber1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter a number 2: ");
+            Totalnumber2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter a number 3: ");
+            Totalnumber3 = Convert.ToDouble(Console.ReadLine());
+
+            total = Totalnumber1 + Totalnumber2 + Totalnumber3;
+            average = total / 3;
+
+            decimal userInput = (decimal)Totalnumber1;
+            userInput = 0.00M;
+
+            Console.WriteLine("Total: " + total);
+            Console.WriteLine("Average number: " + average);
+            Console.WriteLine("Smallest number: " + Math.Min(Totalnumber1, Math.Min(Totalnumber2, Totalnumber3)));
+            Console.WriteLine("Largest number: " + Math.Max(Totalnumber1, Math.Max(Totalnumber2, Totalnumber3)));
+            Console.ReadLine();
+
+            userInput = Convert.ToDecimal(total);
+
+            //Console.WriteLine(userInput.ToString("C"));
+            Console.WriteLine("US: " + userInput.ToString("C"));
+
+            Console.WriteLine("Swedish: " + userInput.ToString("C", CultureInfo.CreateSpecificCulture("sv-SE")));
+
+            Console.WriteLine("Japanese: " + userInput.ToString("C", CultureInfo.CreateSpecificCulture("ja-JP")));
+
+            Console.WriteLine("Thai: " + userInput.ToString("C", CultureInfo.CreateSpecificCulture("th-TH")));
+            Console.ReadLine();
+        }
+    }
+}
